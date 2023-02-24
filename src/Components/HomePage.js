3aa4dddaebd221c183d383a4c1b1fe2e78da614e
saamanthchoutapally <saamanth.choutapally@envisionard.com>
2023-02-25 00:48:25 +0530

    new changes

---
+++ src/Components/HomePage.js
@@ -0,0 +1,12 @@
+import React  from "react";
+const HomePage = () => {
+    return (
+        <>
+        <div className="mainPage">
+            <h1> Feel like celebrity</h1>
+        </div>
+        
+        </>
+    )
+}
+export default HomePage;
